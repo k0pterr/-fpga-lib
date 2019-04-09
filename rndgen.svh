23e3146d9b8e40dfb011af97182efea6793515a9
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2019-04-09 12:16:10 +0700

    make 'svh' wrapper for 'rndgen'

---
+++ rndgen.svh
@@ -0,0 +1,16 @@
+//------------------------------------------------------------------------------
+//  project:       any
+//
+//  packages:      rndgen_pkg
+//
+//  description:   parametrized synthesizable random generator pkg
+//                 see Xilinx xapp 052, xapp 211
+//------------------------------------------------------------------------------
+
+`ifndef RNDGEN_SVH
+`define RNDGEN_SVH
+
+`include "rndgen.pkg"
+
+`endif // RNDGEN_SVH
+
