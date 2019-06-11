c2d0812e18ffa870bd922613b7aee961e2201858
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2019-06-11 12:57:53 +0700

    increase width in 'PRINT_MACRO_STATUS'

--- common.svh
+++ common.svh
@@ -12,9 +12,9 @@
 //------------------------------------------------------------------------------
 `define PRINT_MACRO_STATUS(x,y = "")                \
         `ifdef x                                    \
-            $write("%-40s - ON  %s\n", `"x`", y);   \
+            $write("%-50s - ON  %s\n", `"x`", y);   \
         `else                                       \
-            $write("%-40s - OFF %s\n", `"x`", y);   \
+            $write("%-50s - OFF %s\n", `"x`", y);   \
         `endif
 
 `endif // COMMON_SVH
