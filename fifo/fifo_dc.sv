bd3c1936a78a7bb857f64d605868a2e372ace923
hz <dxp@ngs.ru>
2020-04-20 19:01:43 +0700

    add asymmetric port support for Dual-Clock FIFO

--- fifo/fifo_dc.sv
+++ fifo/fifo_dc.sv
@@ -9,8 +9,6 @@
 `include "common.svh"
 
 //------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
 //
 //   
 //   
@@ -23,33 +21,34 @@
 //
 module fifo_dc_m
 #(
-    parameter type DATA_ITEM_TYPE = logic,
+    parameter type WR_DATA_ITEM_TYPE = logic,
+    parameter type RD_DATA_ITEM_TYPE = logic,
     parameter      DEPTH          = 32,
     parameter      MEMTYPE        = "auto",
     parameter      RELATED_CLOCKS = "no",
     parameter      SYNC_STAGES    = 2
 )
 (
-    input  logic           wr_clk,
-    input  logic           rd_clk,
-    input  logic           rst,
-                           
-    input  DATA_ITEM_TYPE  tail,
-    output DATA_ITEM_TYPE  head,
-    input  logic           push,
-    input  logic           pop,
-    output logic           full,
-    output logic           empty,
-    output logic           wr_rst_busy,
-    output logic           rd_rst_busy
+    input  logic              wr_clk,
+    input  logic              rd_clk,
+    input  logic              rst,
+
+    input  WR_DATA_ITEM_TYPE  tail,
+    output RD_DATA_ITEM_TYPE  head,
+    input  logic              push,
+    input  logic              pop,
+    output logic              full,
+    output logic              empty,
+    output logic              wr_rst_busy,
+    output logic              rd_rst_busy
 );
 
 //------------------------------------------------------------------------------
 //    Settings
 //
 localparam logic RELCLK = RELATED_CLOCKS == "yes" ? 1 : 0;  
-        
-    
+
+
 //------------------------------------------------------------------------------
 //    Types
 //------------------------------------------------------------------------------
@@ -64,25 +63,25 @@ localparam logic RELCLK = RELATED_CLOCKS == "yes" ? 1 : 0;
 //
 xpm_fifo_async 
 #(
-    .FIFO_MEMORY_TYPE    ( MEMTYPE               ),
-    .CDC_SYNC_STAGES     ( SYNC_STAGES           ),
-    .READ_MODE           ( "fwft"                ),
-    .RELATED_CLOCKS      ( RELCLK                ),
-    .FIFO_WRITE_DEPTH    ( DEPTH                 ),
-    .WRITE_DATA_WIDTH    ( $bits(DATA_ITEM_TYPE) ),
-    .READ_DATA_WIDTH     ( $bits(DATA_ITEM_TYPE) ),
-    .USE_ADV_FEATURES    ( "0000"                ),  // all advanced features disabled
-    .DOUT_RESET_VALUE    ( "0"                   ),
-    .ECC_MODE            ( "no_ecc"              ),
-    .FIFO_READ_LATENCY   ( 0                     ),
-    .WAKEUP_TIME         ( 0                     )
-                           
-//  .WR_DATA_COUNT_WIDTH ( 1                     ),
-//  .RD_DATA_COUNT_WIDTH ( 1                     ),
-//  .PROG_FULL_THRESH    ( 10                    ),
-//  .PROG_EMPTY_THRESH   ( 10                    ),
-//  .FULL_RESET_VALUE    ( 0                     ),
-                           
+    .FIFO_MEMORY_TYPE    ( MEMTYPE                  ),
+    .CDC_SYNC_STAGES     ( SYNC_STAGES              ),
+    .READ_MODE           ( "fwft"                   ),
+    .RELATED_CLOCKS      ( RELCLK                   ),
+    .FIFO_WRITE_DEPTH    ( DEPTH                    ),
+    .WRITE_DATA_WIDTH    ( $bits(WR_DATA_ITEM_TYPE) ),
+    .READ_DATA_WIDTH     ( $bits(RD_DATA_ITEM_TYPE) ),
+    .USE_ADV_FEATURES    ( "0000"                   ),  // all advanced features disabled
+    .DOUT_RESET_VALUE    ( "0"                      ),
+    .ECC_MODE            ( "no_ecc"                 ),
+    .FIFO_READ_LATENCY   ( 0                        ),
+    .WAKEUP_TIME         ( 0                        )
+                                                    
+//  .WR_DATA_COUNT_WIDTH ( 1                        ),
+//  .RD_DATA_COUNT_WIDTH ( 1                        ),
+//  .PROG_FULL_THRESH    ( 10                       ),
+//  .PROG_EMPTY_THRESH   ( 10                       ),
+//  .FULL_RESET_VALUE    ( 0                        ),
+
 ) 
 fifo_instance
 (
