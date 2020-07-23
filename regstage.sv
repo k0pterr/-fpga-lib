8430b830d3d27e3514d43c664cbbf421d40c66c5
hz <dxp@ngs.ru>
2020-07-23 18:59:06 +0700

    add 2-stage cut comb path register slice

--- regstage.sv
+++ regstage.sv
@@ -97,4 +97,69 @@ end
 
 endmodule : reg_stage_m
 //------------------------------------------------------------------------------
+module reg_stage2_m
+    #(
+        parameter type DATA_T = logic
+     )
+(
+    input  logic   clk,
+
+    input  logic   valid_src,
+    output logic   ready_src,
+    input  DATA_T  src,
+
+    output logic   valid_dst,
+    input  logic   ready_dst,
+    output DATA_T  dst
+);
+
+//  objects
+logic  full_a = 0;
+logic  full_b = 0;
+logic  wr_a;
+logic  rd_a;
+logic  wr_b;
+logic  rd_b;
+DATA_T data_a;
+DATA_T data_b;
+
+//  logic
+
+always_comb begin
+    wr_a = valid_src && ready_src;
+    rd_a = full_a && !full_b;
+    
+    wr_b = rd_a && !ready_dst;
+    rd_b = full_b && ready_dst;
+end
+
+always_comb begin
+    ready_src = !full_a || !full_b;
+    valid_dst = full_a || full_b;
+    dst       = full_b ? data_b : data_a;
+end
+
+
+always_ff @(posedge clk) begin
+    if(wr_a) begin
+        data_a <= src;
+    end
+    
+    if(wr_a || rd_a) begin
+        full_a <= wr_a;
+    end
+end
+
+always_ff @(posedge clk) begin
+    if(wr_b) begin
+        data_b <= data_a;
+    end
+    
+    if(wr_b || rd_b) begin
+        full_b <= wr_b;
+    end
+end
+
+endmodule : reg_stage2_m
+//------------------------------------------------------------------------------
 
